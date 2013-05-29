8fb8bb3909b238c4c61a271d8dee60bcd05a2305
Alejandro Tapia <ageofzetta@gmail.com>
2013-05-29 18:45:35 -0400

    Corrected one misspelling, changed 3 colors and changed how "italic" was translated

---
+++ src/locales/bootstrap-wysihtml5.es-MX.js
@@ -0,0 +1,49 @@
+/**
+ * Mexican spanish translation for bootstrap-wysihtml5
+ */
+
+(function ($) {
+        $.fn.wysihtml5.locale["es-MX"] = {
+            font_styles: {
+                normal: "Texto normal",
+                h1: "Título 1",
+                h2: "Título 2",
+                h3: "Título 3"
+            },
+            emphasis: {
+                bold: "Negrita",
+                italic: "Cursiva",
+                underline: "Subrayado"
+            },
+            lists: {
+                unordered: "Lista desordenada",
+                ordered: "Lista ordenada",
+                outdent: "Eliminar sangría",
+                indent: "Agregar sangría"
+            },
+            link: {
+                insert: "Insertar enlace",
+                cancel: "Cancelar"
+            },
+            image: {
+                insert: "Insertar imagen",
+                cancel: "Cancelar"
+            },
+            html: {
+                edit: "Editar HTML"
+            },
+            colours: {
+                black: "Negro",
+                silver: "Plateado",
+                gray: "Gris",
+                maroon: "Rojo oscuro",
+                red: "Rojo",
+                purple: "Púrpura",
+                green: "Verde",
+                olive: "Verde oliva",
+                navy: "Azul Marino",
+                blue: "Azul",
+                orange: "Naranja"
+            }
+        };
+    }(jQuery));
